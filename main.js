6a21b1a7e05ae48efdae5537709cfad4fb18a8b3
GuikiPT <72317733+GuikiPT@users.noreply.github.com>
2023-04-04 10:41:33 +0100

    Create main handler to handle __dirname/handlers/events.js handler

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 require('dotenv').config();
 const moment = require('moment');
 const colors = require('colors/safe');
+const fs = require('fs');
 require('better-logging')(console, {
     format: ctx => `${colors.grey('[' + moment().format('LT') + ']')} ${colors.grey('[' + moment().format('L') + ']')} ${ctx.type} >> ${ctx.msg}`,
     saveToFile: `${__dirname}/logs/${moment().format('YYYY')}/${moment().format('MM')}/${moment().format('DD')}.log`,
@@ -21,6 +22,15 @@ const client = new Discord.Client({
     intents: [
         Discord.GatewayIntentBits.Guilds,
     ],
+});
+
+['events'].forEach(handler => {
+    try {
+        require(__dirname + '/handlers/' + handler)(client);
+    }
+    catch (error) {
+        console.error(colors.red(error.stack || error));
+    }
 });
 
 client.once(Discord.Events.ClientReady, async (c) => {
