dbfbba4731638ed59d853f9424d5cbd7c2dddc2b
GuikiPT <72317733+GuikiPT@users.noreply.github.com>
2023-04-04 10:29:07 +0100

    Configured better-logging for bot
    using moment and colors packages for configuration

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 const Discord = require('discord.js');
 require('dotenv').config();
+const moment = require('moment');
+const colors = require('colors/safe');
+require('better-logging')(console, {
+    format: ctx => `${colors.grey('[' + moment().format('LT') + ']')} ${colors.grey('[' + moment().format('L') + ']')} ${ctx.type} >> ${ctx.msg}`,
+    saveToFile: `${__dirname}/logs/${moment().format('YYYY')}/${moment().format('MM')}/${moment().format('DD')}.log`,
+    color: {
+        base: colors.grey,
+        type: {
+            debug: colors.green,
+            info: colors.white,
+            log: colors.grey,
+            error: colors.red,
+            warn: colors.yellow,
+        }
+    },
+});
 
 const client = new Discord.Client({
     intents: [
