e796492fac72ad1c3783c6c6d54e6f11ebad5ec8
GuikiPT <72317733+GuikiPT@users.noreply.github.com>
2023-04-04 11:55:34 +0100

    Added more Intents and also partials

--- main.js
+++ main.js
@@ -20,7 +20,35 @@ require('better-logging')(console, {
 
 const client = new Discord.Client({
     intents: [
+        Discord.GatewayIntentBits.AutoModerationConfiguration,
+        Discord.GatewayIntentBits.AutoModerationExecution,
+        Discord.GatewayIntentBits.DirectMessageReactions,
+        Discord.GatewayIntentBits.DirectMessages,
+        Discord.GatewayIntentBits.GuildEmojisAndStickers,
+        Discord.GatewayIntentBits.GuildIntegrations,
+        Discord.GatewayIntentBits.GuildInvites,
+        Discord.GatewayIntentBits.GuildMembers,
+        Discord.GatewayIntentBits.GuildMessageReactions,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.GuildModeration,
+        Discord.GatewayIntentBits.GuildPresences,
+        Discord.GatewayIntentBits.GuildScheduledEvents,
+        Discord.GatewayIntentBits.GuildVoiceStates,
+        Discord.GatewayIntentBits.GuildWebhooks,
         Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.MessageContent,
+        // Not necessary
+        // Discord.GatewayIntentBits.DirectMessageTyping,
+        // Discord.GatewayIntentBits.GuildMessageTyping,
+    ],
+    partials: [
+        Discord.Partials.Channel,
+        Discord.Partials.GuildMember,
+        Discord.Partials.GuildScheduledEvent,
+        Discord.Partials.Message,
+        Discord.Partials.Reaction,
+        Discord.Partials.ThreadMember,
+        Discord.Partials.User,
     ],
 });
 client.slashCommands = new Discord.Collection();
